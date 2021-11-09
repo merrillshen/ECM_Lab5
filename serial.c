373d23e0fc3e46c53c887a3922d0a769e7720bff
Merrill Shen <50651325+merrillshen@users.noreply.github.com>
2021-11-09 09:48:19 +0000

    Ex 1 T1 Complete: Accept characters from the PC and print them to the LCD screen

--- serial.c
+++ serial.c
@@ -5,6 +5,18 @@ void initUSART4(void) {
 
 	//code to set up USART4 for Reception and Transmission =
 	//see readme for detials
+    RC0PPS = 0x12; // Map EUSART4 TX to RC0
+    RX4PPS = 0x11; // RX is RC1
+    TRISCbits.TRISC1=1; //set TRIS value for pin (input)
+    
+    BAUD4CONbits.BRG16 = 0; 	//set baud rate scaling
+    TX4STAbits.BRGH = 0; 		//high baud rate select bit
+    SP4BRGL = 103; 			//set baud rate to 103 = 9600bps
+    SP4BRGH = 0;			//not used
+
+    RC4STAbits.CREN = 1; 		//enable continuos reception
+    TX4STAbits.TXEN = 1; 		//enable transmitter
+    RC4STAbits.SPEN = 1; 		//enable serial port
 }
 
 //function to wait for a byte to arrive on serial port and read it once it does 
