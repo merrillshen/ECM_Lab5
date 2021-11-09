985dbaf1769060bec26f4f6ba61dab5c5db2dadb
Merrill Shen <50651325+merrillshen@users.noreply.github.com>
2021-11-09 09:50:01 +0000

    Ex 1 T2 Complete: Recalculate and set the baud rate generator value for a baud rate of 19200

--- serial.c
+++ serial.c
@@ -11,7 +11,7 @@ void initUSART4(void) {
     
     BAUD4CONbits.BRG16 = 0; 	//set baud rate scaling
     TX4STAbits.BRGH = 0; 		//high baud rate select bit
-    SP4BRGL = 103; 			//set baud rate to 103 = 9600bps
+    SP4BRGL = 51; 			//set baud rate to 103 = 9600bps
     SP4BRGH = 0;			//not used
 
     RC4STAbits.CREN = 1; 		//enable continuos reception
